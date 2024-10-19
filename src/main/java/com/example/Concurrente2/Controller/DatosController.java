b9ace3efbd603500ca822000dcf115491c2b0ae8
dxn1l <danielmorenorey2012@gmail.com>
2024-10-19 19:48:21 +0200

    Structure

--- src/main/java/com/example/Concurrente2/Controller/DatosController.java
+++ src/main/java/com/example/Concurrente2/Controller/DatosController.java
@@ -3,6 +3,7 @@ package com.example.Concurrente2.Controller;
 
 import com.example.Concurrente2.Entity.Datos;
 import com.example.Concurrente2.Repository.DatosRepository;
+import com.example.Concurrente2.Service.DatosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +18,17 @@ public class DatosController {
     @Autowired
     private DatosRepository datosRepository;
 
+
     @GetMapping
     public List<Datos> getDatos() {
         List<Datos> datos = datosRepository.findAll();
 
-        // Verificación con log de los datos que se están recuperando
+
         datos.forEach(d -> System.out.println(d.toString()));
 
         return datos;
     }
+
 }
 
 
--- src/main/java/com/example/Concurrente2/Controller/FrontendController.java
+++ src/main/java/com/example/Concurrente2/Controller/FrontendController.java
@@ -1,4 +1,4 @@
-package com.example.Concurrente2;
+package com.example.Concurrente2.Controller;
 
 
 import org.springframework.stereotype.Controller;
